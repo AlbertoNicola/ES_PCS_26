e755e0dbfb60340bb763b12a77887a47de96ba0f
AlbertoNicola <s325700@studenti.polito.it>
2026-03-11 10:03:05 +0000

    Correzioni Media_T.cpp

--- esercitazione1/Media_T.cpp
+++ esercitazione1/Media_T.cpp
@@ -2,18 +2,18 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, const char *argv[]) {
-    std::ifstream file(argv[1]);
-    std::string citta;
-    double t1, t2, t3, t4;
-
-
-    while (file >> citta >> t1 >> t2 >> t3 >> t4) {
-        double media = (t1 + t2 + t3 + t4) / 4.0;
-
-        std::cout << citta << " " << media << "\n";
-    }
-
-    file.close();
-    return 0;
+int main(int argc, const char *argv[])
+{
+	std::ifstream file(argv[1]);
+	std::string citta;
+	double t1, t2, t3, t4;
+	
+	while (file >> citta >> t1 >> t2 >> t3 >> t4)
+	{
+		double media = (t1 + t2 + t3 + t4) / 4.0;
+		std::cout << citta << " " << media << "\n";
+		}
+		
+	file.close();
+	return 0;
 }
